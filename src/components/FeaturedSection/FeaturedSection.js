c13ff05e75a092981187fd57e82a2721f5389bdc
pakday <pakdays303@gmail.com>
2022-08-15 21:15:57 +0500

    first commit

--- src/components/FeaturedSection/FeaturedSection.js
+++ src/components/FeaturedSection/FeaturedSection.js
@@ -13,24 +13,24 @@ const FeaturedSection = () => {
                     <div className='card card1'>
                         <h4>Supervisor</h4>
                         <p>Monitors activity to identify project roadblocks</p>
-                        <img src='/images/icon-supervisor.svg' alt='Supervisor Service' />
+                        <img src={process.env.PUBLIC_URL + '/images/icon-supervisor.svg'} alt='Supervisor Service' />
                     </div>
                     <div>
                         <div className='card card2'>
                             <h4>Team Builder</h4>
                             <p>Scans our talent network to create the optimal team for your project</p>
-                            <img src='/images/icon-team-builder.svg' alt='Supervisor Service' />
+                            <img src={process.env.PUBLIC_URL + '/images/icon-team-builder.svg'} alt='Supervisor Service' />
                         </div>
                         <div className='card card3'>
                             <h4>Karma</h4>
                             <p>Regularly evaluates our talent to ensure quality</p>
-                            <img src='/images/icon-karma.svg' alt='Supervisor Service' />
+                            <img src={process.env.PUBLIC_URL + '/images/icon-karma.svg'} alt='Supervisor Service' />
                         </div>
                     </div>
                     <div className='card card4'>
                         <h4>Calculator</h4>
                         <p>Uses data from past projects to provide better delivery estimates</p>
-                        <img src='/images/icon-calculator.svg' alt='Supervisor Service' />
+                        <img src={process.env.PUBLIC_URL + '/images/icon-calculator.svg'} alt='Supervisor Service' />
                     </div>
                 </div>
             </section>

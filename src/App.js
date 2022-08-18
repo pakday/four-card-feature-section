80cf279c11ba0db2e15ce9be8b633c5c8357751d
pakday <pakdays303@gmail.com>
2022-08-18 21:53:12 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import FeaturedSection from "./components/FeaturedSection/FeaturedSection";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <FeaturedSection />
+      <Routes>
+        <Route path="/" element={<h1>Hi</h1>} />
+        <Route path="four-card-feature-section" element={<FeaturedSection />} />
+      </Routes>
     </>
   );
 }
